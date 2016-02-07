47b0a358dca52fbb2dcd46ef5ea2d0f33d8a04ef
Chris Hatch <tofu@redlen.tl>
2016-02-07 10:39:41 +0700

    also skip WiFi.begin if RUN_LOCAL

--- SenseHCMC.ino
+++ SenseHCMC.ino
@@ -47,20 +47,22 @@ void setup() {
   randomSeed(analogRead(0));
   Serial.begin(115200);
 
-  delay(10);
-  Serial.print("Connecting to ");
-  Serial.println(WIFI_SSID);
+  if (!RUN_LOCAL) {
+    delay(10);
+    Serial.print("Connecting to ");
+    Serial.println(WIFI_SSID);
+    
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
   
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    Serial.println("");
+    Serial.println("WiFi connected");  
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
   }
-
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
 void loop() {
